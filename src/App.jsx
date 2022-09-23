714632a90df322b21d0b867ccd34f89a8a601b9e
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-23 05:20:31 +0200

    add news letter component and use useFetcher Hook

--- src/App.jsx
+++ src/App.jsx
@@ -13,35 +13,72 @@ import DeferredBlogPostsPage, {
 } from "./pages/DeferredBlogPosts";
 import ErrorPage from "./pages/Error";
 import NewPostPage, { action as newPostAction } from "./pages/NewPost";
+import { action as newsletterAction } from "./pages/Newsletter";
 import PostDetailPage, { loader as blogPostLoader } from "./pages/PostDetail";
 import RootLayout from "./pages/RootLayout";
 import WelcomePage from "./pages/Welcome";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
-      <Route index element={<WelcomePage />} />
-      <Route path="/blog" element={<BlogLayout />}>
-        {/* <Route index element={<BlogPostsPage />} loader={blogPostsLoader} /> */}
-        <Route
-          index
-          element={<DeferredBlogPostsPage />}
-          loader={deferredBlogPostsLoader}
-        />
-        <Route
-          path=":id"
-          element={<PostDetailPage />}
-          loader={blogPostLoader}
-        />
-      </Route>
-      <Route
-        path="/blog/new"
-        element={<NewPostPage />}
-        action={newPostAction}
-      />
-    </Route>
-  )
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <WelcomePage /> },
+      {
+        path: "/blog",
+        element: <BlogLayout />,
+        children: [
+          {
+            index: true,
+            element: <DeferredBlogPostsPage />,
+            loader: deferredBlogPostsLoader,
+          },
+          {
+            path: ":id",
+            element: <PostDetailPage />,
+            loader: blogPostLoader,
+          },
+        ],
+      },
+      {
+        path: "/blog/new",
+        element: <NewPostPage />,
+        action: newPostAction,
+      },
+    ],
+  },
+  {
+    path: "/newsletter",
+    action: newsletterAction,
+  },
+]);
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
+//       <Route index element={<WelcomePage />} />
+//       <Route path="/blog" element={<BlogLayout />}>
+//         {/* <Route index element={<BlogPostsPage />} loader={blogPostsLoader} /> */}
+//         <Route
+//           index
+//           element={<DeferredBlogPostsPage />}
+//           loader={deferredBlogPostsLoader}
+//         />
+//         <Route
+//           path=":id"
+//           element={<PostDetailPage />}
+//           loader={blogPostLoader}
+//         />
+//       </Route>
+//       <Route
+//         path="/blog/new"
+//         element={<NewPostPage />}
+//         action={newPostAction}
+//       />
+//     </Route>
+//   )
+// );
 
 function App() {
   return (
--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router-dom";
-import BlogPost from "../components/BlogPost";
 
+import BlogPost from "../components/BlogPost";
+import NewsletterSignup from "../components/NewsletterSignup";
 import { getPost } from "../util/api";
 
 function PostDetailPage() {
@@ -8,6 +9,7 @@ function PostDetailPage() {
   return (
     <>
       <BlogPost title={postData.title} text={postData.body} />
+      <NewsletterSignup />
     </>
   );
 }
